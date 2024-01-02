753c4e1addd4227ce998aef1e159d805437c8973
akiva1132 <akiva1132@gmail.com>
2024-01-02 15:30:04 +0200

    add time sign

--- src/producer.ts
+++ src/producer.ts
@@ -3,7 +3,7 @@ import { producer } from "./configurations/kafka";
 export const send = async (message: string) => {
     try {
         await producer.connect()
-        await producer.send({ topic: "missileDataPSI-1", messages: [{ key:"alert", value: message }] });
+        await producer.send({ topic: "missileDataPSI-1", messages: [{ key:Date.now().toString(), value: message }] });
     } catch (error) {
         return Promise.reject(error);
     }
